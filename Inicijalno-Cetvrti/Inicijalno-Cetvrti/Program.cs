e506c4be2b1ecd35d20fc2faecca4c0f35aeb292
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-16 23:42:38 +0200

    Add files via upload

---
+++ Inicijalno-Cetvrti/Inicijalno-Cetvrti/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_Cetvrti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 0;
+            int z = 5;
+
+            if (x>0 && y>0 && z>0)
+            {
+                if ((z*z) == (x*x) + (y*y))
+                {
+                    Console.WriteLine("Trokut je pravokutan.");
+                }
+                else
+                {
+                    Console.WriteLine("Trokut nije pravokutan.");
+                }
+            }  
+            
+            else
+            {
+                Console.WriteLine("Stranice ne mogu biti 0, niti manje od 0!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
